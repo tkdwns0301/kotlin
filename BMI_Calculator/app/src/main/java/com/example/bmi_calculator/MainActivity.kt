ff8b2d94ec57b52a6451dbfd4ef4a28578375c73
hand <tkdwns0301@gmail.com>
2021-07-22 21:10:31 +0900

    [Add] Activity
    2021_07_22

--- BMI_Calculator/app/src/main/java/com/example/bmi_calculator/MainActivity.kt
+++ BMI_Calculator/app/src/main/java/com/example/bmi_calculator/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.example.bmi_calculator
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val heightEditText: EditText = findViewById(R.id.heightEditText) //명시적
+        val weightEditText = findViewById<EditText>(R.id.weightEditText) //추론적
+        val resultButton = findViewById<Button>(R.id.resultButton)
+
+        resultButton.setOnClickListener {
+            Log.d("MainActivity", "ResultButton이 클릭되었습니다.")
+
+            if(heightEditText.text.isEmpty() || weightEditText.text.isEmpty()) {
+                Toast.makeText(this, "빈 값이 있습니다.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            //이 아래로는 절대 빈 값이 올 수 없음
+            val height: Int = heightEditText.text.toString().toInt()
+            val weight: Int = weightEditText.text.toString().toInt()
+
+            Log.d("MainActivity", "height: $height, weight: $weight")
+
+            //https://developer.android.com/guide/components/intents-filters?hl=ko
+            val intent = Intent(this, ResultActivity::class.java)
+            intent.putExtra("height", height)
+            intent.putExtra("weight", weight)
+            startActivity(intent)
+        }
+
+
     }
 }
