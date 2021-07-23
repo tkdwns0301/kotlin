a79e6ee702354883951696500972b2590499d968
hand <tkdwns0301@gmail.com>
2021-07-23 20:29:31 +0900

    [Add] lotto
    2021_07_23

--- lotto/app/src/main/java/com/example/lotto/MainActivity.kt
+++ lotto/app/src/main/java/com/example/lotto/MainActivity.kt
@@ -2,10 +2,127 @@ package com.example.lotto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.NumberPicker
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
 
 class MainActivity : AppCompatActivity() {
+
+    private val clearButton: Button by lazy {
+        findViewById<Button>(R.id.clearButton)
+    }
+    private val addButton: Button by lazy {
+        findViewById<Button>(R.id.addButton)
+    }
+    private val runButton: Button by lazy {
+        findViewById<Button>(R.id.runButton)
+    }
+    private val numberPicker: NumberPicker by lazy { 
+        findViewById<NumberPicker>(R.id.numberPicker)
+    }
+    private val numberTextViewList: List<TextView> by lazy {
+        listOf<TextView>(
+            findViewById<TextView>(R.id.tv1),
+            findViewById<TextView>(R.id.tv2),
+            findViewById<TextView>(R.id.tv3),
+            findViewById<TextView>(R.id.tv4),
+            findViewById<TextView>(R.id.tv5),
+            findViewById<TextView>(R.id.tv6),
+        )
+    }
+    private var didRun = false
+
+    private val pickNumSet = hashSetOf<Int>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        numberPicker.minValue = 1
+        numberPicker.maxValue = 45
+
+        initRunButton()
+        initAddButton()
+        initClearButton()
+    }
+
+    private fun initRunButton() {
+        runButton.setOnClickListener {
+            val list = getRandomNumber()
+            didRun = true
+
+            list.forEachIndexed { index, number ->
+                val textView = numberTextViewList[index]
+
+                textView.text = number.toString()
+                textView.isVisible = true
+
+                setNumberBackground(number, textView)
+            }
+
+        }
+    }
+
+    private fun getRandomNumber(): List<Int> {
+        val numberList = mutableListOf<Int>()
+            .apply {
+                for (i in 1..45) {
+                    if (pickNumSet.contains(i)) {
+                        continue
+                    }
+                    this.add(i)
+                }
+            }
+        numberList.shuffle()
+
+        return pickNumSet.toList() + numberList.subList(0, 6 - pickNumSet.size).sorted()
+    }
+
+    private fun initAddButton() {
+        addButton.setOnClickListener {
+            if (didRun) {
+                Toast.makeText(this, R.string.clearError, Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            if (pickNumSet.size >= 5) {
+                Toast.makeText(this, R.string.sizeError, Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            if (pickNumSet.contains(numberPicker.value)) {
+                Toast.makeText(this, R.string.overlapError, Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            val textView = numberTextViewList[pickNumSet.size]
+            textView.isVisible = true
+            textView.text = numberPicker.value.toString()
+
+            setNumberBackground(numberPicker.value, textView)
+
+            pickNumSet.add(numberPicker.value)
+        }
+    }
+
+    private fun initClearButton() {
+        clearButton.setOnClickListener {
+            pickNumSet.clear()
+            numberTextViewList.forEach {
+                it.isVisible = false
+            }
+            didRun = false
+        }
+    }
+
+    private fun setNumberBackground(number: Int, textView: TextView) {
+        when(number) {
+            in 1..10 -> textView.background = ContextCompat.getDrawable(this, R.drawable.circle_yellow)
+            in 11..20 -> textView.background = ContextCompat.getDrawable(this, R.drawable.circle_blue)
+            in 21..30 -> textView.background = ContextCompat.getDrawable(this, R.drawable.circle_red)
+            in 31..40 -> textView.background = ContextCompat.getDrawable(this, R.drawable.circle_gray)
+            else -> textView.background = ContextCompat.getDrawable(this, R.drawable.circle_green)
+        }
     }
 }
