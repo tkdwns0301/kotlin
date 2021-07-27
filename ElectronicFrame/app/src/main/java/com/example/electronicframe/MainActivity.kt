781e149f808df49431366a60b66f7b66448ec52e
hand <tkdwns0301@gmail.com>
2021-07-27 20:06:40 +0900

    [Add] Fun Permission allow
    2021_07_27

--- ElectronicFrame/app/src/main/java/com/example/electronicframe/MainActivity.kt
+++ ElectronicFrame/app/src/main/java/com/example/electronicframe/MainActivity.kt
@@ -1,11 +1,62 @@
 package com.example.electronicframe
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
+    private val btnAddPhoto: Button by lazy {
+        findViewById(R.id.btnAddPhoto)
+    }
+
+    private val btnStart: Button by lazy {
+        findViewById(R.id.btnStart)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initBtnAddPhoto()
+        initBtnStart()
+    }
+
+    private fun initBtnAddPhoto() {
+        btnAddPhoto.setOnClickListener {
+            when {
+                ContextCompat.checkSelfPermission(
+                    this,
+                    android.Manifest.permission.READ_EXTERNAL_STORAGE
+                ) == PackageManager.PERMISSION_GRANTED -> {
+                    //TODO 권한이 잘 부여되었을 때, 갤러리에서 사진을 선택하는 기능
+                }
+                shouldShowRequestPermissionRationale(android.Manifest.permission.READ_EXTERNAL_STORAGE) -> {
+                    //TODO 교육용 팝업 확인 후 권한 팝업을 띄우는 기능
+                    showPermissionContextPopup()
+
+                }
+                else -> {
+                    requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1000)
+                }
+            }
+        }
+    }
+
+    private fun showPermissionContextPopup() {
+        AlertDialog.Builder(this)
+            .setTitle("권한이 필요합니다.")
+            .setMessage("전자액자 앱에서 사진을 불러오기 위해 권한이 필요합니다.")
+            .setPositiveButton("동의하기") { _, _ ->
+                requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1000)
+            }
+            .setNegativeButton("취소하기") { _, _ -> }
+            .create()
+            .show()
+    }
+    private fun initBtnStart() {
+
     }
 }
